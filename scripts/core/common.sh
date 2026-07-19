8fe2aac4a4ea4c6fb7d3b90ba1f54f9596d845a7
j7an <j7an@users.noreply.github.com>
2026-07-19 09:21:29 -0700

    refactor: centralize entry workspace cleanup

--- scripts/core/common.sh
+++ scripts/core/common.sh
@@ -18,6 +18,25 @@ spw_require_command() {
   fi
 }
 
+spw_make_workspace() {
+  mktemp -d "$1/$2.XXXXXX"
+}
+
+spw_cleanup_workspace_trap() {
+  _spw_workspace_trap_status=$?
+  trap - 0 HUP INT TERM
+  rm -rf "$_spw_workspace_trap_path" || :
+  exit "$_spw_workspace_trap_status"
+}
+
+spw_install_workspace_trap() {
+  _spw_workspace_trap_path="$1"
+  trap spw_cleanup_workspace_trap 0
+  trap 'exit 129' HUP
+  trap 'exit 130' INT
+  trap 'exit 143' TERM
+}
+
 spw_root() {
   CDPATH= cd -- "$(dirname "$0")/.." && pwd
 }
